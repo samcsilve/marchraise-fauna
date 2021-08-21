c047131c2be9ccb72905310e3fefcf0763993529
samcsilve <samcsilve@gmail.com>
2021-08-21 18:17:00 -0500

    fathom

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import { ApolloProviderWrapper } from "@/utils/apolloClient";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "../styles/globals.css";
 import "@fontsource/poppins/400.css";
+import * as Fathom from 'fathom-client';
+
 
 const theme = extendTheme({
   fonts: {
